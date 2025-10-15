0f4a3661a8ea9f8bffaa157331ebdae03f506bf9
Hanush <112359676+hanush21@users.noreply.github.com>
2025-10-15 11:59:40 +0200

    dd

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -5,6 +5,7 @@ globalThis.__BUILD_MANIFEST = {
   "devFiles": [],
   "ampDevFiles": [],
   "polyfillFiles": [
+<<<<<<< Updated upstream
     "static/chunks/3aeaf_next_dist_build_polyfills_polyfill-nomodule.js"
   ],
   "lowPriorityFiles": [],
@@ -18,6 +19,21 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/3aeaf_@swc_helpers_cjs_f1f0f0ff._.js",
     "static/chunks/Documents_CURSOS_B_ACTIVA_Hackato25_git_TouristMap_a0ff3932._.js",
     "static/chunks/turbopack-Documents_CURSOS_B_ACTIVA_Hackato25_git_TouristMap_edfd464c._.js"
+=======
+    "static/chunks/node_modules_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_57d40746._.js",
+    "static/chunks/node_modules_next_dist_compiled_react-dom_1e674e59._.js",
+    "static/chunks/node_modules_next_dist_compiled_next-devtools_index_a9cb0712.js",
+    "static/chunks/node_modules_next_dist_compiled_5150ccfd._.js",
+    "static/chunks/node_modules_next_dist_client_cf1d9188._.js",
+    "static/chunks/node_modules_next_dist_b0daae9a._.js",
+    "static/chunks/node_modules_@swc_helpers_cjs_b3dc30d6._.js",
+    "static/chunks/_a0ff3932._.js",
+    "static/chunks/turbopack-_cdba956c._.js"
+>>>>>>> Stashed changes
   ],
   "ampFirstPages": []
 };
