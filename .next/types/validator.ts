504e9dba7bab27ac361a6bb2d949fd6149a3b4cc
Hanush <112359676+hanush21@users.noreply.github.com>
2025-10-15 14:03:19 +0200

    gg

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -36,15 +36,6 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
 }
 
 
-// Validate ../../app/map/page.tsx
-{
-  type __IsExpected<Specific extends AppPageConfig<"/map">> = Specific
-  const handler = {} as typeof import("../../app/map/page.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
 // Validate ../../app/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
