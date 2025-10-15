52da0ca9a56827fb0da9e2549859e3fe62080fdb
Hanush <112359676+hanush21@users.noreply.github.com>
2025-10-15 11:59:59 +0200

    dd

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -6,6 +6,7 @@ globalThis.__BUILD_MANIFEST = {
   "ampDevFiles": [],
   "polyfillFiles": [
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     "static/chunks/3aeaf_next_dist_build_polyfills_polyfill-nomodule.js"
   ],
   "lowPriorityFiles": [],
@@ -24,6 +25,12 @@ globalThis.__BUILD_MANIFEST = {
   ],
   "lowPriorityFiles": [],
   "rootMainFiles": [
+=======
+    "static/chunks/node_modules_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+>>>>>>> Stashed changes
     "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_57d40746._.js",
     "static/chunks/node_modules_next_dist_compiled_react-dom_1e674e59._.js",
     "static/chunks/node_modules_next_dist_compiled_next-devtools_index_a9cb0712.js",
@@ -33,6 +40,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/node_modules_@swc_helpers_cjs_b3dc30d6._.js",
     "static/chunks/_a0ff3932._.js",
     "static/chunks/turbopack-_cdba956c._.js"
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
   ],
   "ampFirstPages": []
--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,5 +1,9 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/Documents/CURSOS/B_ACTIVA/Hackato25/git/TouristMap/app/page\": [\n      \"static/media/caa3a2e1cccd8315-s.p.853070df.woff2\",\n      \"static/media/797e433ab948586e-s.p.dbea232f.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
+=======
+self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/app/page\": [\n      \"static/media/caa3a2e1cccd8315-s.p.853070df.woff2\",\n      \"static/media/797e433ab948586e-s.p.dbea232f.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
+>>>>>>> Stashed changes
 =======
 self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/app/page\": [\n      \"static/media/caa3a2e1cccd8315-s.p.853070df.woff2\",\n      \"static/media/797e433ab948586e-s.p.dbea232f.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
 >>>>>>> Stashed changes
